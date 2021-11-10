956e8ad3c917f4ccd88e51e95f5472bda6740840
Thomas van Steen <210742@student.glu.nl>
2021-11-10 10:55:49 +0100

    1

---
+++ header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php bloginfo('name'); ?></title>
+        <link href="<?php bloginfo('template_url'); ?>/css/wildvecht.css" rel="stylesheet" />
+        <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet" />
+    </head>
+<body>
