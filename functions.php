6e6f7b3c62962711198f1750230157dbae63244e
Thomas van Steen <210742@student.glu.nl>
2021-11-10 14:24:27 +0100

    nieuws

--- functions.php
+++ functions.php
@@ -9,8 +9,19 @@ function registeer_widgets() {
     }
     add_action( 'widgets_init', 'registeer_widgets' );
 
+function register_my_menus() {
+    register_nav_menus(
+      array(
+        'header-menu' => __( 'Hoofdmenu' ),
+        'extra-menu' => __( 'Extra Menu' )
+       )
+     );
+   }
+   add_action( 'init', 'register_my_menus' );
+      
 
 
-    
+
+
 
 ?>
