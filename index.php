7b2bcb0c3d1d881c8f0fd9d41b2237534d4864da
Thomas van Steen <210742@student.glu.nl>
2021-11-11 10:25:48 +0100

    final

--- index.php
+++ index.php
@@ -27,17 +27,18 @@
     
     <div id="content">
       <div class="area">
-      <?php if (have_posts()) :
-            while (have_posts()) : the_post(); ?>
-            <header><h1><?php the_title(); ?></h1></header>
-            <p><?php the_content(); ?></p>
-      <?php endwhile;
-              else:
-                echo"<p>Geen data Gevonden</p>";
-              endif;
-
-                
-      ?>
+      <?php if(have_posts()) : while(have_posts()) : the_post() ?>
+            <?php the_title('<h1>','</h1>'); ?>
+            <?php if ( is_single() && 'post' == get_post_type() ): ?>
+                <?php the_content(); ?>
+                <?php if (comments_open() || get_comments_number() ): ?> 
+                    <?php comments_template(); ?>
+                <?php endif; ?>                     
+            <?php else: ?>
+                <?php the_excerpt(); ?>
+                <a href="<?php the_permalink(); ?>">lees meer</a>
+            <?php endif; ?>
+        <?php endwhile; endif; ?>
         
       </div>
     </div>
